88cae57b70491827615661ec17eda19a206c46ce
Lautaro Jordan Lobo Ravarotto <43019989+LautaroLobo12@users.noreply.github.com>
2020-07-28 12:25:07 -0300

    Adding all the files

---
+++ build/precache-manifest.6333e54577822a76fb4388f3a3e83932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da15a123ea789d53e412797bbf5f3723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76141067004bcbee5fd8",
+    "url": "/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "bf7efc272e9beee74e00",
+    "url": "/static/js/2.d9371c6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d9371c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76141067004bcbee5fd8",
+    "url": "/static/js/main.bf3a3657.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
